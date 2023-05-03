676d8c147b87003b0577d5cfc5c4be023fe69ec9
Konstantin <kurakin-1993@mail.ru>
2023-05-03 22:38:05 +0400

    Add project files.

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,50 @@
+﻿/*
+* Запросим пользовательский ввод
+* и сохраним отзыв в объект
+* 
+* */
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+}
+
+/*
+* Запишем отзыв на страницу 
+* 
+* */
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
+
+//let offsset = 0;
+//const sliderLine = document.querySelector('.slide');
+
+//document.querySelector('.navigation').addEventListener('click', function (e) {
+//    if (e.target.closest('.bar'))
+//    {
+//        alert('dsad');
+//        offsset = offsset + 1000;
+//        sliders.style.left = offsset + 'px';
+//    }
+//});
